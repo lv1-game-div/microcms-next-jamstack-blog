d11fce26a7e9369f464d7fd924fd23890bd262d4
福島恵珠 <nesskindbead2@gmail.com>
2022-12-26 00:43:53 +0900

    feat: ソースコードのシンタックスハイライトに対応

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,14 +1,25 @@
 import { client } from "../../libs/client";
 import styles from "../../styles/Home.module.scss";
+import cheerio from 'cheerio';
+import hljs from 'highlight.js'
+import 'highlight.js/styles/vs2015.css';
 
 // microCMSからブログデータid毎に取得
 export const getStaticProps = async (context) => {
   const id = context.params.id;
   const data = await client.get({ endpoint: "blog", contentId: id });
 
+  const $ = cheerio.load(data.body);  // data.contentはmicroCMSから返されるリッチエディタ部
+  $('pre code').each((_, elm) => {
+    const result = hljs.highlightAuto($(elm).text());
+    $(elm).html(result.value);
+    $(elm).addClass('hljs');
+  });
+  data.content = $.html();
+
   return {
     props: {
-      blog: data,
+      blog: data.content,
     },
   };
 };
@@ -29,7 +40,7 @@ export default function BlogId({ blog }) {
       <h1 className={styles.title}>{blog.title}</h1>
       <p className={styles.publishedAtDetail}>{blog.publishedAt}</p>
       <div
-        dangerouslySetInnerHTML={{ __html: `${blog.body}` }}
+        dangerouslySetInnerHTML={{ __html: `${blog}` }}
         className={styles.post}
       ></div>
     </main>
