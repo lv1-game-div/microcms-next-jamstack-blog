1d8f5b038b11403dd91ff13000cd0b6236718384
福島恵珠 <nesskindbead2@gmail.com>
2022-12-25 21:45:25 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { client } from "../libs/client";
+import styles from "../styles/Home.module.scss";
+
+// microCMSからブログデータを取得
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: "blog" });
+  console.log(data);
+  return {
+    props: {
+      blog: data.contents,
+    },
+  };
+};
+
+export default function Home({ blog }) {
+  return (
+    <div className={styles.container}>
+      {blog.map((blog) => (
+        <li className={styles.list} key={blog.id}>
+          <Link className={styles.title} href={`blog/${blog.id}`}>
+            {blog.title}
+          </Link>
+          <p className={styles.publishedAtIndex}>記事投稿日：{new Date(blog.publishedAt).toLocaleDateString()}</p>
+        </li>
+      ))}
+    </div>
+  );
+}
