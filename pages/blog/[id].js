0f56a0728a8cbb0f0257818b804b51621eefae97
福島恵珠 <nesskindbead2@gmail.com>
2022-12-26 14:30:32 +0900

    refactor: cheerioの直接参照は非推奨だったため、loadをimport

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,6 +1,6 @@
 import { client } from "../../libs/client";
 import styles from "../../styles/Home.module.scss";
-import cheerio from 'cheerio';
+import { load } from "cheerio";
 import hljs from 'highlight.js'
 import 'highlight.js/styles/vs2015.css';
 
@@ -9,7 +9,7 @@ export const getStaticProps = async (context) => {
   const id = context.params.id;
   const data = await client.get({ endpoint: "blog", contentId: id });
 
-  const $ = cheerio.load(data.body);  // data.contentはmicroCMSから返されるリッチエディタ部
+  const $ = load(data.body);  // data.contentはmicroCMSから返されるリッチエディタ部
   $('pre code').each((_, elm) => {
     const result = hljs.highlightAuto($(elm).text());
     $(elm).html(result.value);
